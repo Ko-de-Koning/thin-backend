71592ea0835ec64af0b863cae502c665b7b6be3a
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-02-23 12:35:35 +0100

    feat: thin-back-end-try-out

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { initThinBackend } from 'thin-backend';
-import { ThinBackend } from 'thin-backend-react';
 import 'thin-backend-react/auth.css';
+
+import type { AppProps } from 'next/app'
+import { ThinBackend } from 'thin-backend-react';
+import { initThinBackend } from 'thin-backend';
 
 initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
